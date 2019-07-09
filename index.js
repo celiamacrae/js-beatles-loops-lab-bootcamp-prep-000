7fcc4c28da17b55e067751abc240459d29ae9c11
amazing-server-2710 <celiamacrae@gmail.com>
2019-07-09 16:51:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function iLoveTheBeatles(number){
   var beatles = []
   do{
     beatles.push("I love the Beatles!")
-    number = number++
+    number++
   } while (number < 15)
   return beatles
 }
